d75fcd61584bbeb66dd5def018fe254f0423ad91
Vibhor sxn <42259504+vibhorsxn@users.noreply.github.com>
2021-09-29 12:35:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,8 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from './react';
 import Nav from './Navbar';
 import Login from './Login';
 import Register from './Register';
 import Forgot from './Forgot';
-import Home from "./Home";
 import RegisterCus from "./RegisterCus";
 
 function App() {
@@ -14,16 +13,6 @@ function App() {
     <Register/>
     <Forgot/>
     <RegisterCus/>
-    {/* <Router>
-      <Nav />  
-      <Switch>
-      <Route path="/"  exact component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/customer" component={RegisterCus} />
-        <Route path="/vendor" component={Register} />
-        <Route path="/forgot" component={Forgot} />
-      </Switch>
-    </Router> */}
     </>
   );
 }
